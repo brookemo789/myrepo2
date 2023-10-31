ac0027f52fe2130685c3e9addb1655b50113e951
ameliacostantini <amelialili2004@gmail.com>
2023-10-31 13:50:54 -0400

    final edits

--- main.py
+++ main.py
@@ -8,30 +8,27 @@ def print_menu():
 def encode(og_password):
     encoded_password = ''
     for i in range(len(og_password)):
-        new_pass += str((int(og_password[i]))+3)
+        encoded_password += str((int(og_password[i]))+3)
     return encoded_password
 
 def decode(encoded_password = "12345555"):
     og_password = ''
-    for i in range(len(og_password)):
-        og_password += str((int(og_password[i])) -3)
+    for i in range(len(encoded_password)):
+        og_password += str((int(encoded_password[i])) -3)
     return og_password
 
-if __name__ == 'main':
+if __name__ == '__main__':
     loop = True
     while loop:
         print_menu()
         user_choice = int(input("Please enter an option: "))
-        og_password = ""
-        ecoded_password = ""
 
         if user_choice == 1:
-            endcoded_password = input("Pleae enter your password to encode: ")
-            encoded_password = encode(user_password)
+            og_password = input("Pleae enter your password to encode: ")
+            encoded_password = encode(og_password)
             print('Your password has been encoded and stored!')
         elif user_choice == 2:
             og_password = decode(encoded_password)
             print('The encoded password is ' + str(encoded_password) + ', and the original password is ' + str(og_password) + '.')
         elif user_choice == 3:
             break
-
